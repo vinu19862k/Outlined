6a0b9bc8fa4df422069a8b8f888974d6a799e420
vinu19862k <vinu19862k@hotmail.com>
2015-10-05 12:44:01 +0530

    sdf

--- www/js/tools.js
+++ www/js/tools.js
@@ -92,3 +92,23 @@ function addimg(e) {
     $(div).draggable();
     $(div).attr("onclick", "hideall()");
 }
+
+var myElement = document.getElementById('test');
+
+var mc = new Hammer.Manager(myElement);
+
+// create a pinch and rotate recognizer
+// these require 2 pointers
+var pinch = new Hammer.Pinch();
+var rotate = new Hammer.Rotate();
+
+// we want to detect both the same time
+pinch.recognizeWith(rotate);
+
+// add to the Manager
+mc.add([pinch, rotate]);
+
+
+mc.on("pinch rotate", function (ev) {
+    myElement.textContent += ev.type + " ";
+});
